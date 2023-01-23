65a504c07f73be25a4c40ddc49f54ac58b42d9fa
ianowalker <122535918+ianowalker@users.noreply.github.com>
2023-01-23 12:25:21 +0300

    Add files via upload

---
+++ multiplication of two numbers.c
@@ -0,0 +1,15 @@
+//Program of multiplication of two numbers
+#include<stdio.h>
+int main()
+{
+  int a,b,product;
+  printf("\nEnter the first number:");
+  scanf("%d",&a);
+
+  printf("\nEnter the second number:");
+  scanf("%d",&b);
+
+  product=a*b;
+  printf("\nProduct=%d",product);
+  return 0;
+}
