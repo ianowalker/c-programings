9da49f17928595ab012cb39d6244a69ac554103a
ianowalker <122535918+ianowalker@users.noreply.github.com>
2023-02-21 11:55:59 +0300

    Create First 50 natural numbers in reverse form.c

---
+++ First 50 natural numbers in reverse form.c
@@ -0,0 +1,14 @@
+//First 50 natural numbers in reverse order
+#include <stdio.h>
+int main ()
+{
+  int a,num=50;
+printf("\n The 50 natural num in reverse are:");
+  
+  for(a=num;a>=1;a--)
+    {
+ printf("\n%d",a);
+      }
+  
+  return 0;
+}
