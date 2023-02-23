d98452be833dfaac572e1d827d6f336ddf921b5e
ianowalker <122535918+ianowalker@users.noreply.github.com>
2023-02-23 13:57:09 +0300

    Create first 9 natural num.c

---
+++ first 9 natural num.c
@@ -0,0 +1,12 @@
+//first 9 natural numbers in reverse
+#include<stdio.h>
+int main()
+{
+  int i;
+  for(i=9;i>=0;i--)
+    {
+      printf("%d",i);
+    }
+      printf("\nfor loop body finished:");
+  return 0;
+}
