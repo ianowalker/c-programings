d2a4305dd52dd2fe010126e5699ff71a60527402
ianowalker <122535918+ianowalker@users.noreply.github.com>
2023-03-02 14:59:23 +0300

    Create function of volume of a sphere.c

---
+++ function of volume of a sphere.c
@@ -0,0 +1,23 @@
+//fuction of a volume of a sphere
+#include<stdio.h>
+#define pi 3.142
+int area(int r);
+int main()
+{
+    int r,result;
+    printf("\nEnter the radius:");
+    scanf("%d",&r);
+    
+    result=4/3*pi*r*r*r;
+    printf("\nThe volume of a sphere is: %d",result);
+    
+    return 0;
+}
+
+int area(int r)
+{
+    int answer;
+    answer= 4/3*pi*r*r*r;
+    
+    return answer;
+}
